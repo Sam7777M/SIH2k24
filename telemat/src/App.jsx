8c7999deb4fd710a95aa7c6163907ed3b31ab193
Shashwat Mishra <smishra@solveda.com>
2024-12-09 19:43:12 +0530

    Commit 2

--- telemat/src/App.jsx
+++ telemat/src/App.jsx
@@ -3,7 +3,6 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { QrCodeProvider } from "./pages/QrCodeContext";
 import Aopt from "./pages/Aopt";
-import SharedRoute from "./Components/SharedRoute";
 import Live from "./pages/Live";
 import Monitor from "./pages/Monitor";
 import Dashboard1 from "./pages/Dashboard1";
@@ -11,19 +10,15 @@ import Reg1 from "./pages/Reg1";
 import Homepage from "./pages/Homepage";
 import Services from "./pages/Services";
 import Reg2 from "./pages/Reg2";
-import Reg3 from "./pages/Reg3";
 import UserTypeSelection from "./pages/UserTypeSelection";
 import LoginForm from "./pages/LoginForm";
 import Schedule from "./pages/Schedule";
 import InQr from "./pages/InQr";
-import ScheduleReg from "./components/ScheduleReg";
-import Qreg from "./components/Qreg";
+import ScheduleReg from "./Components/ScheduleReg";
+import Qreg from "./Components/Qreg";
 import Dashboard2 from "./pages/Dashboard2";
 import Dashboard3 from "./pages/Dashboard3";
 import Dr1 from "./Components/Dr1";
-import Dr2 from "./Components/Dr2";
-import Dr3 from "./Components/Dr3";
-import Dr4 from "./Components/Dr4";
 import Reg11 from "./pages/Reg11";
 import Reg12 from "./pages/Reg12";
 import Reg13 from "./pages/Reg13";
@@ -62,7 +57,8 @@ function App() {
         <div className="App">
           <Routes>
             {/* Use `element` instead of `component` */}
-            <Route path="/" element={<Services />} />
+            <Route path="/" element={<Homepage />} />
+            <Route path="/choosepartnert" element={<Services />} />
             <Route path="/monitor" element={<Monitor />} />
             <Route path="/services" element={<Services />} />
             <Route path="/aopt" element={<Aopt />} />
@@ -109,6 +105,8 @@ function App() {
             <Route path="/live2" element={<Live2 />} />
             <Route path="/qregt" element={<Qregt />} />
             <Route path="/choosepartnert" element={<ChoosePartnert />} />
+            <Route path="/schedulereg" element={<ScheduleReg />} />
+            <Route path="/choosepartnert" element={<PartnerDataPage />} />
            
             
 
